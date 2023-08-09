99b5a032d355821286ca55cdad4cec41662cd6e0
Tyler Gilroy <tyler.gilroy.23@gmail.com>
2023-08-09 16:08:24 -0600

    styles files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
-import "./App.scss";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import NavBar from "./components/nav/NavBar";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Cart from "./components/pages/Cart";
+import Contact from "./components/pages/Contact";
+import Products from "./components/pages/Products";
+import SingleProduct from "./components/pages/SingleProduct";
+import "./styles/app.scss";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app-body">
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/cart" component={Cart} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/products" component={Products} />
+          <Route exact path="/product/:slug" component={SingleProduct} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,3 +1,20 @@
+import { NavLink } from "react-router-dom";
+
 export default function NavBar() {
-  return <div className="navbar-body"></div>;
+  return (
+    <div className="nav-bar-body">
+      <div className="navbar-title-wrapper">
+        <p>Website Title</p>
+      </div>
+      <div className="navbar-links-wrapper">
+        <NavLink exact to="/">
+          Home
+        </NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/cart">Cart</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+        <NavLink to="/products">Products</NavLink>
+      </div>
+    </div>
+  );
 }
