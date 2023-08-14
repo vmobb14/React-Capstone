8bcfb52fd8cfc4e4651f2db31466a3b434f0689d
Tyler Gilroy <tyler.gilroy.23@gmail.com>
2023-08-14 16:48:08 -0600

    Final touches on Home, NavBar, & Footer

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -7,12 +7,11 @@ export default function Home() {
       </div>
       <div className="home-intro-text">
         <p>
-          Welcome to my React.js capstone program. My name is Tyler Gilroy, and
-          this project bears the results of my blood, sweat, and MANY MANY
-          tears. Is it perfect? Absolutely not. But seven months prior to my
-          creation of this website, I had exactly zero knowledge of software
-          development. Now I can build projects such as this, on my own of my
-          own ability and knowledge.
+          Welcome to my React.js Capstone website. My name is Tyler Gilroy, and
+          this project bears the results of my blood, sweat, and (many) tears.
+          Is it perfect? Absolutely not, but seven months prior to my creation
+          of this website, I had exactly zero knowledge of software development.
+          Now, I can single-handedly build enitre projects such as this.
         </p>
       </div>
     </div>
