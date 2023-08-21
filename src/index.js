0bd578564a3a491f5bb0bbd914816f41152c67fd
Tyler Gilroy <tyler.gilroy.23@gmail.com>
2023-08-21 13:52:10 -0600

    Handled console errors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
